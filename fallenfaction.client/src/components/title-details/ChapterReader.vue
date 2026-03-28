b9ada43461450c7dc3e9cd6d904c7628316243bf
Serh Yue <mdjdjzx@gmail.com>
2026-03-28 18:26:16 +0100

    next

--- fallenfaction.client/src/components/title-details/ChapterReader.vue
+++ fallenfaction.client/src/components/title-details/ChapterReader.vue
@@ -27,6 +27,12 @@
           </div>
         </div>
         <div class="navbar-right">
+          <button v-if="isAuthenticated && chapterData?.id" class="icon-btn" @click="showReportModal = true" title="Report this chapter">
+            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
+              <path d="M4 15s1-1 4-1 5 2 8 2 4-1 4-1V3s-1 1-4 1-5-2-8-2-4 1-4 1z" />
+              <line x1="4" y1="22" x2="4" y2="15" />
+            </svg>
+          </button>
           <button class="icon-btn" @click="toggleSettings">
             <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round">
               <path d="M12.22 2h-.44a2 2 0 0 0-2 2v.18a2 2 0 0 1-1 1.73l-.43.25a2 2 0 0 1-2 0l-.15-.08a2 2 0 0 0-2.73.73l-.22.38a2 2 0 0 0 .73 2.73l.15.1a2 2 0 0 1 1 1.72v.51a2 2 0 0 1-1 1.74l-.15.09a2 2 0 0 0-.73 2.73l.22.38a2 2 0 0 0 2.73.73l.15-.08a2 2 0 0 1 2 0l.43.25a2 2 0 0 1 1 1.73V20a2 2 0 0 0 2 2h.44a2 2 0 0 0 2-2v-.18a2 2 0 0 1 1-1.73l.43-.25a2 2 0 0 1 2 0l.15.08a2 2 0 0 0 2.73-.73l.22-.39a2 2 0 0 0-.73-2.73l-.15-.08a2 2 0 0 1-1-1.74v-.5a2 2 0 0 1 1-1.74l.15-.09a2 2 0 0 0 .73-2.73l-.22-.38a2 2 0 0 0-2.73-.73l-.15.08a2 2 0 0 1-2 0l-.43-.25a2 2 0 0 1-1-1.73V4a2 2 0 0 0-2-2z" />
@@ -223,6 +229,16 @@
       </div>
     </div>
   </div>
+
+  <!-- Report Chapter Modal -->
+  <ReportModal
+    v-if="chapterData?.id && isAuthenticated"
+    :is-open="showReportModal"
+    :target-type="3"
+    :target-id="chapterData.id"
+    @close="showReportModal = false"
+    @reported="showReportModal = false"
+  />
 </template>
 
 <script setup>
@@ -231,6 +247,7 @@
   import { titleDetailsService } from '../../services/titleDetailsService'
   import { chapterService } from '../../services/chapterService'
   import CommentsComponent from '../title-details/CommentsComponent.vue'
+  import ReportModal from '../shared/ReportModal.vue'
   import { buildTitleSlug } from '@/utils/titleSlug.js'
 
   const props = defineProps({
@@ -249,6 +266,7 @@
   const chapterData = ref(null)
   const chaptersList = ref([])
   const isAuthenticated = ref(false)
+  const showReportModal = ref(false)
   const currentUserId = ref('')
   const isAdmin = ref(false)
 
