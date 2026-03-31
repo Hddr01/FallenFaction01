8d320dd97560a9d5255a6c6d006aded5286ca291
Serh Yue <mdjdjzx@gmail.com>
2026-03-31 20:42:58 +0200

    psuh

---
+++ FallenFaction.Server/Migrations/20260331183033_nemmig.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace FallenFaction.Server.Migrations
+{
+    /// <inheritdoc />
+    public partial class nemmig : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PendingChapters_Chapters_OriginalChapterId",
+                table: "PendingChapters");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PendingChapters_Chapters_OriginalChapterId",
+                table: "PendingChapters",
+                column: "OriginalChapterId",
+                principalTable: "Chapters",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_PendingChapters_Chapters_OriginalChapterId",
+                table: "PendingChapters");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_PendingChapters_Chapters_OriginalChapterId",
+                table: "PendingChapters",
+                column: "OriginalChapterId",
+                principalTable: "Chapters",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.SetNull);
+        }
+    }
+}
